cff3b3d76314913b519235c3f30e2b708ca0174a
lucas xavier <lucassx1995@gmail.com>
2021-02-22 14:07:44 -0300

    Configurar solicitação de permissões

--- app/src/main/java/com/example/mensageiro/activity/ConfiguracoesActivity.java
+++ app/src/main/java/com/example/mensageiro/activity/ConfiguracoesActivity.java
@@ -1,18 +1,33 @@
 package com.example.mensageiro.activity;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import com.example.mensageiro.R;
+import com.example.mensageiro.helper.Permissao;
 
 public class ConfiguracoesActivity extends AppCompatActivity {
+
+    private String[] permissoesNecessarias = new String[] {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuracoes);
+
+
+        //Validar permissões
+        Permissao.validarPermissões(permissoesNecessarias, this, 1);
 
         //configurando toolbar
         Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
@@ -22,4 +37,31 @@ public class ConfiguracoesActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
     } //fim do onCreate
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        for(int permissaoResultado : grantResults) {
+            if(permissaoResultado == PackageManager.PERMISSION_DENIED) {
+                alertaValidacaoPermissao();
+            }
+        }
+    } //fim do método onRequestPermissionsResult
+
+    private void alertaValidacaoPermissao() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Permissões negadas");
+        builder.setMessage("Para utilizar o aplicativo é necessário aceitar as permissões");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    } //fim do método alertaValidacaoPermissao
 }
