73024bde74f1c72e1b8de644860f948889452623
lucas xavier <lucassx1995@gmail.com>
2021-02-25 08:24:26 -0300

    listagem de contatos

--- app/src/main/java/com/example/mensageiro/fragment/ContatosFragment.java
+++ app/src/main/java/com/example/mensageiro/fragment/ContatosFragment.java
@@ -2,7 +2,9 @@ package com.example.mensageiro.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
@@ -10,66 +12,84 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.mensageiro.R;
+import com.example.mensageiro.adapter.ContatosAdapter;
+import com.example.mensageiro.config.ConfiguracaoFirebase;
+import com.example.mensageiro.helper.ClassesDatabase;
+import com.example.mensageiro.model.Usuario;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
-/*/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ContatosFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
 
 public class ContatosFragment extends Fragment {
 
     private RecyclerView recyclerViewListaContatos;
-
-    /*// TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-     */
+    private ContatosAdapter adapter;
+    private ArrayList<Usuario> listaContatos = new ArrayList<>();
+    private DatabaseReference usuariosRef;
+    private ValueEventListener valueEventListenerContatos;
 
     public ContatosFragment() {
         // Required empty public constructor
     }
 
-    /*/**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ContatosFragment.
-     */
-    /*// TODO: Rename and change types and number of parameters
-    public static ContatosFragment newInstance(String param1, String param2) {
-        ContatosFragment fragment = new ContatosFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-     */
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contatos, container, false);
-        //View inflater.inflate(R.layout.fragment_contatos, container, false);
+        //return inflater.inflate(R.layout.fragment_contatos, container, false);
+        View view = inflater.inflate(R.layout.fragment_contatos, container, false);
+
+        //configurações iniciais
+        recyclerViewListaContatos = view.findViewById(R.id.recyclerViewListaContatos);
+        usuariosRef = ConfiguracaoFirebase.getFirebaseDatabase().child(ClassesDatabase.USUARIOS);
+
+
+        //configurar adapter
+        adapter = new ContatosAdapter(listaContatos, getActivity());
+
+        //configurar recyclerview
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerViewListaContatos.setLayoutManager(layoutManager);
+        recyclerViewListaContatos.setHasFixedSize(true);
+        recyclerViewListaContatos.setAdapter(adapter);
+
+        return view;
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        recuperarContatos();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        usuariosRef.removeEventListener(valueEventListenerContatos);
+    }
+
+    public void recuperarContatos() {
+        valueEventListenerContatos = usuariosRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                for(DataSnapshot dados : snapshot.getChildren()) {
+                    Usuario usuario = dados.getValue(Usuario.class);
+                    listaContatos.add(usuario);
+                }
+
+                adapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    } //fim recuperarContatos
 }
---
+++ app/src/main/java/com/example/mensageiro/helper/ClassesDatabase.java
@@ -0,0 +1,8 @@
+package com.example.mensageiro.helper;
+
+public class ClassesDatabase {
+    public static final String USUARIOS = "usuarios";
+    public static final String USUARIO_NOME = "nome";
+    public static final String USUARIO_EMAIL = "email";
+    public static final String USUARIO_FOTO = "foto";
+}
